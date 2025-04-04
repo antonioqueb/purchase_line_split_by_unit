b2397791bf469f056c99da6ba99381477af43d36
Antonio Queb <antonioqueb@alphaqueb.online>
2025-04-03 22:10:38 -0600

    Up

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -4,6 +4,9 @@ class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
     sku_unico = fields.Char(string="SKU Único", readonly=True)
+    x_lote_comun = fields.Char(string='Lote común')
+    x_sku_prefix = fields.Char(string='Prefijo SKU')
+
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
@@ -15,7 +18,14 @@ class StockPicking(models.Model):
             line_counter = {}
             for move_line in picking.move_line_ids_without_package.filtered(lambda l: l.quantity == 1 and l.lot_id and not l.sku_unico):
                 purchase_line = move_line.move_id.purchase_line_id
-                prefix = purchase_line.x_sku_prefix or move_line.lot_id.name
+                if not purchase_line:
+                    continue
+
+                # Heredar campos a move_line
+                move_line.x_lote_comun = purchase_line.x_lote_comun
+                move_line.x_sku_prefix = purchase_line.x_sku_prefix
+
+                prefix = move_line.x_sku_prefix or move_line.lot_id.name
                 lot_name = move_line.lot_id.name
 
                 if prefix not in line_counter:
