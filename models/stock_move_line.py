64bc12b8662074c77e72ba4b43d0e9fc76c5983f
Antonio Queb <antonioqueb@alphaqueb.online>
2025-04-04 19:15:06 -0600

    Up

--- models/stock_move_line.py
+++ models/stock_move_line.py
@@ -1,8 +1,19 @@
-from odoo import models, fields
+from odoo import api
 
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    unique_sku = fields.Char(string='SKU único')
-    common_lot = fields.Char(string='Lote común')
-    sku_prefix = fields.Char(string='Prefijo SKU')
+    @api.model_create_multi
+    def create(self, vals_list):
+        move_lines = super().create(vals_list)
+        for line in move_lines:
+            if line.picking_id and line.picking_id.picking_type_code == 'incoming':
+                quants = self.env['stock.quant'].search([
+                    ('product_id', '=', line.product_id.id),
+                    ('location_id', '=', line.location_dest_id.id),
+                    ('lot_id', '=', line.lot_id.id if line.lot_id else False),
+                ])
+                for quant in quants:
+                    quant.common_lot = line.common_lot
+                    quant.sku_prefix = line.sku_prefix
+        return move_lines
--- models/stock_quant.py
+++ models/stock_quant.py
@@ -4,5 +4,5 @@ from odoo import models, fields
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    common_lot = fields.Char(string='Lote común', related='lot_id.common_lot', store=True)
-    sku_prefix = fields.Char(string='Prefijo SKU', related='lot_id.sku_prefix', store=True)
+    common_lot = fields.Char(string='Lote común')
+    sku_prefix = fields.Char(string='Prefijo SKU')
