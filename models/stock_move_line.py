3735004f316e180b2f9d55bd00212947717d8530
Antonio Queb <antonioqueb@alphaqueb.online>
2025-04-04 19:19:43 -0600

    Up

--- models/stock_move_line.py
+++ models/stock_move_line.py
@@ -3,6 +3,10 @@ from odoo import models, fields, api
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
+    unique_sku = fields.Char(string='SKU único')
+    common_lot = fields.Char(string='Lote común')
+    sku_prefix = fields.Char(string='Prefijo SKU')
+
     @api.model_create_multi
     def create(self, vals_list):
         move_lines = super().create(vals_list)
