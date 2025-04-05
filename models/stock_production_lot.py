7effdaff5e1ac35de49786cfa4150375a881880e
Antonio Queb <antonioqueb@alphaqueb.online>
2025-04-04 19:05:20 -0600

    Up

--- models/stock_production_lot.py
+++
@@ -1,8 +0,0 @@
-# models/stock_production_lot.py
-from odoo import models, fields
-
-class StockProductionLot(models.Model):
-    _inherit = 'stock.production.lot'
-
-    common_lot = fields.Char(string='Lote común')
-    sku_prefix = fields.Char(string='Prefijo SKU')
